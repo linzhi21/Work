cebd89be305d7adf9e4e5d4028fbab3f8d52f780
韩璐 <hanlu@csvw.com>
2021-05-12 15:39:29 +0800

    更新 environment.prod.js

--- environment.prod.js
+++ environment.prod.js
@@ -1,73 +1,24 @@
 export default {
-    // systemUrl: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc',
-    // /** 用户模块 */
-    // oauth2: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/oauth2',
-    // /** 系统设置模块 */
-    // system: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/system',
-    // /** 设备模块 */
-    // device: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/device/api/device',
-    // /** 备件模块 */
-    // spare: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/spare/',
-    // /** 商业模块 */
-    // business: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/business',
-    // /** 审批模块 */
-    // approval: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/approval',
-    // /** 附件管理 */
-    // accessory: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/accessory',
-    // // /** 资料库 */
-    // database: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/database',
-    // /** 文件服务器地址 */
-    // accessoryFile: 'http://10.160.245.70:9000/tpms',
-    // log: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/log',  // 日志地址
-
-/* ==================================测试======================================= */
-    systemUrl: 'http://tpms-gateway-runtime.ci-testapps.ocp.sitc',
+    systemUrl: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc',
     /** 用户模块 */
-    oauth2: 'http://tpms-gateway-runtime.ci-testapps.ocp.sitc/oauth2',
+    oauth2: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/oauth2',
     /** 系统设置模块 */
-    system: 'http://tpms-gateway-runtime.ci-testapps.ocp.sitc/system',
+    system: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/system',
     /** 设备模块 */
-    device: 'http://tpms-gateway-runtime.ci-testapps.ocp.sitc/device/api/device',
+    device: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/device/api/device',
     /** 备件模块 */
-    spare: 'http://tpms-gateway-runtime.ci-testapps.ocp.sitc/spare/',
+    spare: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/spare/',
     /** 商业模块 */
-    business: 'http://tpms-gateway-runtime.ci-testapps.ocp.sitc/business',
+    business: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/business',
     /** 审批模块 */
-    approval: 'http://tpms-gateway-runtime.ci-testapps.ocp.sitc/approval',
+    approval: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/approval',
     /** 附件管理 */
-    accessory: 'http://tpms-gateway-runtime.ci-testapps.ocp.sitc/accessory',
-    /** 资料库 */
-    database: 'http://tpms-gateway-runtime.ci-testapps.ocp.sitc/database',
-    /** 专家智库 */
-    expert: 'http://tpms-gateway-runtime.ci-testapps.ocp.sitc/expert',
+    accessory: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/accessory',
+    // /** 资料库 */
+    database: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/database',
     /** 文件服务器地址 */
-    accessoryFile: 'http://10.160.244.189:9000/tpms',
-    log: 'http://tpms-gateway-runtime.ci-testapps.ocp.sitc/log',  // 日志地址
-/* ==================================外网======================================= */
-    // systemUrl: 'http://47.116.138.243:9010',
-    // /** 用户模块 */
-    // oauth2: 'http://47.116.138.243:9010/tpms/oauth2',  //服务器地址
-    // // oauth2: 'http://10.0.0.86:9000/tpms/oauth2',     //jackbo地址
-    // /** 系统设置模块 */
-    // system: 'http://47.116.138.243:9010/tpms/system',  //服务器地址
-    // // system: 'http://10.0.0.86:9000/tpms/system',     //jackbo地址
-    // /** 设备模块 */
-    // device: 'http://47.116.138.243:9010/tpms/device/api/device',  //服务器地址
-    // // device: 'http://10.0.0.86:9000/tpms/device/api/device',     //jackbo地址
-    // /** 备件模块 */
-    // spare: 'http://47.116.138.243:9010/tpms/spare/',  //服务器地址
-    // // spare: 'http://10.0.0.86:9000/tpms/spare/api',     //jackbo地址
-    // /** 点检模块*/
-    // business: 'http://47.116.138.243:9010/tpms/business',  //服务器地址
-    // // business: 'http://10.0.0.86:9000/tpms/business',          //jackbo地址
-    // /** 审批模块 */
-    // approval: 'http://47.116.138.243:9010/tpms/approval',  //服务器地址
-    // // approval: 'http://10.0.0.86:9000/tpms/approval',     //jackbo地址
-    // /** 附件管理 */
-    // accessory: 'http://47.116.138.243:9010/tpms/accessory',  //服务器地址
-    // /** 文件服务器地址 */
-    // accessoryFile: 'http://122.112.190.19:9000/tpms',
-    // log: 'http://47.116.138.243:9010/tpms/log',  // 日志地址
+    accessoryFile: 'http://10.160.245.70:9000/tpms',
+    log: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/log',  // 日志地址
 
 /* ==================================SAP账号密码======================================= */
     sap: 'http://svlpod01.csvw.com:50000/RESTAdapter/rest/dongchang/ZMDCIF_TPMS_MATKC', // SAP 备件库存接口
