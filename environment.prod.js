843fa1b4e1a3eed180db24733216469b5480bda3
mozziechu <mozziechu@kanducloud.com>
2021-11-10 10:51:52 +0800

    *)版本号修复;

--- environment.prod.js
+++ environment.prod.js
@@ -1,48 +1,48 @@
 export default {
-    systemUrl: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc',
-    /** 用户模块 */
-    oauth2: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/oauth2',
-    /** 系统设置模块 */
-    system: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/system',
-    /** 设备模块 */
-    device: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/device/api/device',
-    /** 备件模块 */
-    spare: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/spare/',
-    /** 商业模块 */
-    business: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/business',
-    /** 审批模块 */
-    approval: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/approval',
-    /** 附件管理 */
-    accessory: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/accessory',
-    // /** 资料库 */
-    database: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/database',
-    /** 文件服务器地址 */
-    accessoryFile: 'http://10.160.245.70:9000/tpms',
-    log: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/log',  // 日志地址
+    // systemUrl: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc',
+    // /** 用户模块 */
+    // oauth2: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/oauth2',
+    // /** 系统设置模块 */
+    // system: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/system',
+    // /** 设备模块 */
+    // device: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/device/api/device',
+    // /** 备件模块 */
+    // spare: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/spare/',
+    // /** 商业模块 */
+    // business: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/business',
+    // /** 审批模块 */
+    // approval: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/approval',
+    // /** 附件管理 */
+    // accessory: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/accessory',
+    // // /** 资料库 */
+    // database: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/database',
+    // /** 文件服务器地址 */
+    // accessoryFile: 'http://10.160.245.70:9000/tpms',
+    // log: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/log',  // 日志地址
 
 /* ==================================测试======================================= */
-    // systemUrl: 'http://tpms-gateway-runtime.ci-testapps.ocp.sitc',
-    // /** 用户模块 */
-    // oauth2: 'http://tpms-gateway-runtime.ci-testapps.ocp.sitc/oauth2',
-    // /** 系统设置模块 */
-    // system: 'http://tpms-gateway-runtime.ci-testapps.ocp.sitc/system',
-    // /** 设备模块 */
-    // device: 'http://tpms-gateway-runtime.ci-testapps.ocp.sitc/device/api/device',
-    // /** 备件模块 */
-    // spare: 'http://tpms-gateway-runtime.ci-testapps.ocp.sitc/spare/',
-    // /** 商业模块 */
-    // business: 'http://tpms-gateway-runtime.ci-testapps.ocp.sitc/business',
-    // /** 审批模块 */
-    // approval: 'http://tpms-gateway-runtime.ci-testapps.ocp.sitc/approval',
-    // /** 附件管理 */
-    // accessory: 'http://tpms-gateway-runtime.ci-testapps.ocp.sitc/accessory',
-    // /** 资料库 */
-    // database: 'http://tpms-gateway-runtime.ci-testapps.ocp.sitc/database',
-    // /** 专家智库 */
-    // expert: 'http://tpms-gateway-runtime.ci-testapps.ocp.sitc/expert',
-    // /** 文件服务器地址 */
-    // accessoryFile: 'http://10.160.244.247:9000/tpms',
-    // log: 'http://tpms-gateway-runtime.ci-testapps.ocp.sitc/log',  // 日志地址
+    systemUrl: 'http://tpms-gateway-runtime.ci-testapps.ocp.sitc',
+    /** 用户模块 */
+    oauth2: 'http://tpms-gateway-runtime.ci-testapps.ocp.sitc/oauth2',
+    /** 系统设置模块 */
+    system: 'http://tpms-gateway-runtime.ci-testapps.ocp.sitc/system',
+    /** 设备模块 */
+    device: 'http://tpms-gateway-runtime.ci-testapps.ocp.sitc/device/api/device',
+    /** 备件模块 */
+    spare: 'http://tpms-gateway-runtime.ci-testapps.ocp.sitc/spare/',
+    /** 商业模块 */
+    business: 'http://tpms-gateway-runtime.ci-testapps.ocp.sitc/business',
+    /** 审批模块 */
+    approval: 'http://tpms-gateway-runtime.ci-testapps.ocp.sitc/approval',
+    /** 附件管理 */
+    accessory: 'http://tpms-gateway-runtime.ci-testapps.ocp.sitc/accessory',
+    /** 资料库 */
+    database: 'http://tpms-gateway-runtime.ci-testapps.ocp.sitc/database',
+    /** 专家智库 */
+    expert: 'http://tpms-gateway-runtime.ci-testapps.ocp.sitc/expert',
+    /** 文件服务器地址 */
+    accessoryFile: 'http://10.160.244.247:9000/tpms',
+    log: 'http://tpms-gateway-runtime.ci-testapps.ocp.sitc/log',  // 日志地址
 /* ==================================外网======================================= */
     // systemUrl: 'http://47.116.138.243:9010',
     // /** 用户模块 */
