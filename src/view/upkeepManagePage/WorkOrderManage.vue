953163f59e887699e91be74db8962d1a01a05a4a
mozziechu <mozziechu@kanducloud.com>
2022-11-14 10:34:52 +0800

    保养计划负责人参数调整;

--- src/view/upkeepManagePage/WorkOrderManage.vue
+++ src/view/upkeepManagePage/WorkOrderManage.vue
@@ -142,6 +142,7 @@ import {
   importThisMonthMaintainWorkOrder,
   approvalMaintainWorkorder
 } from "../../lib/api/upkeepManagePage";
+import { getUserDropList } from '../../lib/api/userManage'
 import { tpmsHeader, tpmsTable, tpmsChoosefile } from "../../components";
 import {
   factoryManage,
@@ -206,7 +207,7 @@ export default {
             { label: "过期又启动", id: "2" },
           ],
         },
-        { label: "计划负责人", props: "creator", value: "", placeholder: "导出设备保养概览时，请填写!" },
+        { label: "计划负责人", props: "creatorName", value: "", placeholder: "导出设备保养概览时，请填写!" },
         { label: "设备名称", props: "deviceName", value: "" },
         { label: "设备编号", props: "deviceNo", value: "" },
         { label: "资产编号", props: "deviceAssetNo", value: "" },
@@ -315,11 +316,24 @@ export default {
     getData() {
       let data = this.$refs.tpmsHeader.getData();
       let pageData = this.$refs.tpmsTable.getData();
-      console.log(data);
-      maintainWorkOrder({ ...data, ...pageData }).then((res) => {
-        this.listData = res.data.content;
-        this.total = res.data.totalElements;
-      });
+      if (data.creatorName != null && data.creatorName) {
+        this.getUserDropList(data.creatorName).then(res => {
+          const { content } = res.data;
+          data.creator = content[0].id
+          maintainWorkOrder({ ...data, ...pageData }).then((res) => {
+            this.listData = res.data.content;
+            this.total = res.data.totalElements;
+          });
+        }).catch(err => {
+          this.$message.error(`用户查询失败！${err}`);
+        });
+      } else {
+        maintainWorkOrder({ ...data, ...pageData }).then((res) => {
+          this.listData = res.data.content;
+          this.total = res.data.totalElements;
+        });
+      }
+
     },
     /** 头部value变更回调 */
     onValueChanged({ props, value }) {
@@ -571,6 +585,16 @@ export default {
       }).catch(err => {
         this.$message.error('审批失败！');
       })
+    },
+    getUserDropList(query) {
+      // getUserDropList({ name: query }).then(res => {
+      //   const { content } = res.data;
+      //   const { equipmentFormList } = this;
+      //   equipmentFormList.push({props: 'creator', value: content[0].id})
+      // }).catch(err => {
+      //   this.$message.error(`用户查询失败！${err}`);
+      // });
+      return getUserDropList({ name: query })
     }
   },
 };
