61df651889fe7ae6b11092d72e42eb27266a950c
xuwenping <xuwenping@imonsoft.com>
2023-02-15 11:34:51 +0800

    prod配置合并

--- environment.prod.js
+++ environment.prod.js
@@ -1,50 +1,26 @@
 export default {
-    systemUrl: 'http://tpms-gateway-runtime.apps.ci-test.ocp.sitc',
+    systemUrl: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc',
     /** 用户模块 */
-    oauth2: 'http://tpms-gateway-runtime.apps.ci-test.ocp.sitc/oauth2',
+    oauth2: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/oauth2',
     /** 系统设置模块 */
-    system: 'http://tpms-gateway-runtime.apps.ci-test.ocp.sitc/system',
+    system: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/system',
     /** 设备模块 */
-    device: 'http://tpms-gateway-runtime.apps.ci-test.ocp.sitc/device',
+    device: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/device',
     /** 备件模块 */
-    spare: 'http://tpms-gateway-runtime.apps.ci-test.ocp.sitc/spare/',
+    spare: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/spare/',
     /** 商业模块 */
-    business: 'http://tpms-gateway-runtime.apps.ci-test.ocp.sitc/business',
+    business: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/business',
     /** 审批模块 */
-    approval: 'http://tpms-gateway-runtime.apps.ci-test.ocp.sitc/approval',
+    approval: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/approval',
     /** 附件管理 */
-    accessory: 'http://tpms-gateway-runtime.apps.ci-test.ocp.sitc/accessory',
-    // /** 资料库 */
-    database: 'http://tpms-gateway-runtime.apps.ci-test.ocp.sitc/database',
-    // /** 专家智库 */
-    expert: 'http://tpms-gateway-runtime.apps.ci-test.ocp.sitc/expert',
+    accessory: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/accessory',
+    /** 资料库 */
+    database: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/database', 
+    /** 专家智库 */
+    expert: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/expert', 
     /** 文件服务器地址 */
-    accessoryFile: 'http://10.160.244.247:9000/tpms',
-    log: 'http://tpms-gateway-runtime.apps.ci-test.ocp.sitc/log',  // 日志地址
-
-/* ==================================测试======================================= */
-    // systemUrl: 'http://tpms-gateway-runtime.ci-testapps.ocp.sitc',
-    // /** 用户模块 */
-    // oauth2: 'http://tpms-gateway-runtime.ci-testapps.ocp.sitc/oauth2',
-    // /** 系统设置模块 */
-    // system: 'http://tpms-gateway-runtime.ci-testapps.ocp.sitc/system',
-    // /** 设备模块 */
-    // device: 'http://tpms-gateway-runtime.ci-testapps.ocp.sitc/device/api/device',
-    // /** 备件模块 */
-    // spare: 'http://tpms-gateway-runtime.ci-testapps.ocp.sitc/spare/',
-    // /** 商业模块 */
-    // business: 'http://tpms-gateway-runtime.ci-testapps.ocp.sitc/business',
-    // /** 审批模块 */
-    // approval: 'http://tpms-gateway-runtime.ci-testapps.ocp.sitc/approval',
-    // /** 附件管理 */
-    // accessory: 'http://tpms-gateway-runtime.ci-testapps.ocp.sitc/accessory',
-    // /** 资料库 */
-    // database: 'http://tpms-gateway-runtime.ci-testapps.ocp.sitc/database',
-    // /** 专家智库 */
-    // expert: 'http://tpms-gateway-runtime.ci-testapps.ocp.sitc/expert',
-    // /** 文件服务器地址 */
-    // accessoryFile: 'http://10.160.244.247:9000/tpms',
-    // log: 'http://tpms-gateway-runtime.ci-testapps.ocp.sitc/log',  // 日志地址
+    accessoryFile: 'http://10.160.245.70:9000/tpms',
+    log: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/log',  // 日志地址
 
 /* ==================================SAP账号密码======================================= */
     sap: 'http://svlpod01.csvw.com:50000/RESTAdapter/rest/dongchang/ZMDCIF_TPMS_MATKC', // SAP 备件库存接口
