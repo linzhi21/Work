7b019b163a39eba2d208f7bf2e79b078abccd89e
xuwenping <xuwenping@imonsoft.com>
2023-03-02 10:14:09 +0800

    服务器迁移

--- environment.prod.js
+++ environment.prod.js
@@ -1,26 +1,26 @@
 export default {
-    systemUrl: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc',
+    systemUrl: 'http://tpms-gateway-runtime.apps.ci-prod.ocp.sitc',
     /** 用户模块 */
-    oauth2: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/oauth2',
+    oauth2: 'http://tpms-gateway-runtime.apps.ci-prod.ocp.sitc/oauth2',
     /** 系统设置模块 */
-    system: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/system',
+    system: 'http://tpms-gateway-runtime.apps.ci-prod.ocp.sitc/system',
     /** 设备模块 */
-    device: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/device',
+    device: 'http://tpms-gateway-runtime.apps.ci-prod.ocp.sitc/device',
     /** 备件模块 */
-    spare: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/spare/',
+    spare: 'http://tpms-gateway-runtime.apps.ci-prod.ocp.sitc/spare/',
     /** 商业模块 */
-    business: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/business',
+    business: 'http://tpms-gateway-runtime.apps.ci-prod.ocp.sitc/business',
     /** 审批模块 */
-    approval: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/approval',
+    approval: 'http://tpms-gateway-runtime.apps.ci-prod.ocp.sitc/approval',
     /** 附件管理 */
-    accessory: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/accessory',
+    accessory: 'http://tpms-gateway-runtime.apps.ci-prod.ocp.sitc/accessory',
     /** 资料库 */
-    database: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/database', 
+    database: 'http://tpms-gateway-runtime.apps.ci-prod.ocp.sitc/database', 
     /** 专家智库 */
-    expert: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/expert', 
+    expert: 'http://tpms-gateway-runtime.apps.ci-prod.ocp.sitc/expert', 
     /** 文件服务器地址 */
     accessoryFile: 'http://10.160.245.70:9000/tpms',
-    log: 'http://tpms-gateway-runtime.ci-prodapps.ocp.sitc/log',  // 日志地址
+    log: 'http://tpms-gateway-runtime.apps.ci-prod.ocp.sitc/log',  // 日志地址
 
 /* ==================================SAP账号密码======================================= */
     sap: 'http://svlpod01.csvw.com:50000/RESTAdapter/rest/dongchang/ZMDCIF_TPMS_MATKC', // SAP 备件库存接口
